bc08811cd6fc01165fb77a44ad4fe57ca789a821
kao <25aw0114@jec.ac.jp>
2025-12-17 16:17:40 +0900

    add about page and home page

--- 1210-react/src/page/About.jsx
+++ 1210-react/src/page/About.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <h1>About</h1>
+        </>
     )
 }
 
-export default About
+export default About;
